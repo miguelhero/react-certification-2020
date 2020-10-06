8a7d175f7d8526a504a20905f6e6dd43eced588f
Miguel H Ortiz <miguel.ortiz@wizeline.com>
2020-10-06 11:41:11 -0500

    Fix broken test with latest changes

--- src/components/SearchVideo/Search.component.test.jsx
+++ src/components/SearchVideo/Search.component.test.jsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import SearchVideo from './Search.component';
 import { useSearch } from '../../providers/Search';
 
@@ -12,7 +13,7 @@ describe('Search component', () => {
   it('should hold the value entered on change', () => {
     const mockedFunction = jest.fn();
     useSearch.mockImplementation(() => ['', mockedFunction]);
-    render(<SearchVideo />);
+    render(<SearchVideo />, { wrapper: MemoryRouter });
     const searchInput = screen.getByRole('textbox');
     fireEvent.change(searchInput, { target: { value: 'cool videos' } });
     expect(searchInput.value).toBe('cool videos');
@@ -21,7 +22,7 @@ describe('Search component', () => {
   it('should call search context on submit', () => {
     const mockedFunction = jest.fn();
     useSearch.mockImplementation(() => ['', mockedFunction]);
-    render(<SearchVideo />);
+    render(<SearchVideo />, { wrapper: MemoryRouter });
     const searchInput = screen.getByRole('textbox');
     fireEvent.submit(searchInput, { target: { value: 'cool videos' } });
     expect(mockedFunction).toHaveBeenCalledTimes(1);
