a4087aad78f450d13f252c49272761013c91ab07
Miguel H Ortiz <miguel.ortiz@wizeline.com>
2020-10-06 11:31:08 -0500

    Fix to redirect to home when search

--- src/components/SearchVideo/Search.component.jsx
+++ src/components/SearchVideo/Search.component.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { Input, InputAdornment } from '@material-ui/core';
 import { Search as SearchIcon } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 import { useSearch } from '../../providers/Search';
 
 const SearchVideo = () => {
+  const history = useHistory();
   const [searchValue, setSearchValue] = useState('');
   const [, setSearchQuery] = useSearch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchQuery(searchValue);
+    history.push('/');
   };
 
   return (
